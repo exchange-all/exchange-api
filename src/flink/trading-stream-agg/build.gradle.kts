1d02b2330fb55f2c92201c60b656c9932de6848f
uuhnaut69 <uuhnaut96@gmail.com>
2023-12-18 20:26:50 +0700

    feat: Init flink job

---
+++ src/flink/trading-stream-agg/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "com.exchange"
+version = "1.0-RELEASE"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/flink/trading-stream-agg/src/main/java/com/exchange/job/TradingStreamAggregation.java
@@ -0,0 +1,10 @@
+package com.exchange.job;
+
+/**
+ * @author uuhnaut69
+ */
+public class TradingStreamAggregation {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

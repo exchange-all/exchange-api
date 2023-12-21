bf6dce22f0b8f71594fa6789ae71b1526f2d0600
thaivc <giacatluong20121996@gmail.com>
2023-12-21 22:40:17 +0700

    fix: edit test

--- src/orderbook/src/test/kotlin/com/exchange/orderbook/service/MatchingEngineTest.kt
+++ src/orderbook/src/test/kotlin/com/exchange/orderbook/service/MatchingEngineTest.kt
@@ -1,68 +1,80 @@
 package com.exchange.orderbook.service
 
+import com.exchange.orderbook.model.entity.OrderEntity
+import com.exchange.orderbook.model.entity.OrderType
+import com.exchange.orderbook.model.entity.Status
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 import java.util.*
 
 /**
  * @author thaivc
  * @since 2023
  */
-class TreeNode(var priority: Int, var value: Int) : Comparable<TreeNode> {
-    override fun compareTo(other: TreeNode): Int {
-        return this.priority.compareTo(other.priority)
-    }
-
-}
 
 class MatchingEngineTest {
 
     @Test
     fun `test matching one head-bid and one head-ask `() {
-        val asks = TreeSet<TreeNode>()
+        val asks: LinkedHashMap<String, OrderEntity> = LinkedHashMap()
+
         // 10, 50, 30, 20, 40
-        asks.addAll(
-            listOf(
-                TreeNode(0, 5),
-                TreeNode(1, 10),
-                TreeNode(2, 50),
-                TreeNode(3, 30),
-                TreeNode(4, 20),
-                TreeNode(5, 40)
-            )
-        )
-        val bids = TreeSet<TreeNode>()
+        listOf(10, 50, 30, 20, 40).forEach {
+            val id = UUID.randomUUID().toString()
+            asks[id] = OrderEntity().apply {
+                this.id = id
+                this.amount = BigDecimal(it)
+                this.availableAmount = BigDecimal(it)
+                this.price = BigDecimal.ONE
+                this.status = Status.OPEN
+                userId = UUID.randomUUID().toString()
+                tradingPairId = UUID.randomUUID().toString()
+                type = OrderType.SELL
+            }
+        }
+
+        val bids: LinkedHashMap<String, OrderEntity> = LinkedHashMap()
         // 25, 40, 10
-        bids.addAll(listOf(TreeNode(0, 5), TreeNode(1, 25), TreeNode(2, 40), TreeNode(3, 10)))
+        listOf(25, 40, 10).forEach {
+            val id = UUID.randomUUID().toString()
+            bids[id] = OrderEntity().apply {
+                this.id = id
+                this.amount = BigDecimal(it)
+                this.availableAmount = BigDecimal(it)
+                this.price = BigDecimal.ONE
+                this.status = Status.OPEN
+                userId = UUID.randomUUID().toString()
+                tradingPairId = UUID.randomUUID().toString()
+                type = OrderType.BUY
+            }
+        }
 
         while (true) {
             if (asks.isEmpty() || bids.isEmpty()) {
                 println("END TEST!!!")
-                println("remain-asks: ${asks.map { it.value }}")
-                println("remain-bids: $bids")
+                println("remain-asks: ${jacksonObjectMapper().writeValueAsString(asks)}")
+                println("remain-bids: ${jacksonObjectMapper().writeValueAsString(bids)}")
                 return
             }
-            val askHead = asks.first()
-            val bidHead = bids.first()
+            val askHead = asks.entries.first().value
+            val bidHead = bids.entries.first().value
 
-            if (askHead.value > bidHead.value) {
-                println("askHead: ${askHead.value} > bidHead: ${bidHead.value} | remain askHead: ${askHead.value - bidHead.value}")
-                bids.remove(bidHead)
-                askHead.value -= bidHead.value
-                bidHead.value = 0
-            }
-            if (askHead.value < bidHead.value) {
-                println("askHead: ${askHead.value} < bidHead: ${bidHead.value} | remain bidHead: ${bidHead.value - askHead.value}")
-                asks.remove(askHead)
-                bidHead.value -= askHead.value
-                askHead.value = 0
+            val tradedQuoteAmount = minOf(askHead.availableAmount, bidHead.availableAmount)
+
+            print("ask ${askHead.availableAmount}, bid ${bidHead.availableAmount} -> traded: $tradedQuoteAmount -> ")
+
+            askHead.availableAmount = askHead.availableAmount.subtract(tradedQuoteAmount)
+            bidHead.availableAmount = bidHead.availableAmount.subtract(tradedQuoteAmount)
+
+            println("ask ${askHead.availableAmount}, bid ${bidHead.availableAmount}")
+
+            if (askHead.availableAmount <= BigDecimal.ZERO) {
+                asks.remove(askHead.id)
             }
 
-            if (askHead.value == bidHead.value) {
-                println("askHead: ${askHead.value} == bidHead: ${bidHead.value}")
-                asks.remove(askHead)
-                bids.remove(bidHead)
-                askHead.value = 0
-                bidHead.value = 0
+            if (bidHead.availableAmount <= BigDecimal.ZERO) {
+                bids.remove(bidHead.id)
             }
         }
     }
